e9ecb3759a52524557f28a37c92e899e8cc32bb1
rangemaster <rangemaster_r@hotmail.com>
2014-12-25 00:36:37 +0100

    Set-up mainpage

---
+++ Bus_Game/Bus_Game/Bus_Game.Windows/MainPage.xaml.cs
@@ -0,0 +1,69 @@
+﻿using Bus_Game.Common;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Basic Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234237
+
+namespace Bus_Game
+{
+    /// <summary>
+    /// A basic page that provides characteristics common to most applications.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+
+        private NavigationHelper navigationHelper;
+        private ObservableDictionary defaultViewModel = new ObservableDictionary();
+
+        public MainPage()
+        {
+            this.InitializeComponent();
+            this.navigationHelper = new NavigationHelper(this);
+            this.navigationHelper.LoadState += navigationHelper_LoadState;
+            this.navigationHelper.SaveState += navigationHelper_SaveState;
+            Init();
+        }
+        private void Init()
+        {
+            this.Bots_rb.IsChecked = true;
+            this._AmountOfPlayers_cb.Items.Add("1");
+        }
+
+        #region NavigationHelper registration
+        private void navigationHelper_LoadState(object sender, LoadStateEventArgs e)
+        {
+        }
+        private void navigationHelper_SaveState(object sender, SaveStateEventArgs e)
+        {
+        }
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        { navigationHelper.OnNavigatedTo(e); }
+        protected override void OnNavigatedFrom(NavigationEventArgs e)
+        { navigationHelper.OnNavigatedFrom(e); }
+        public ObservableDictionary DefaultViewModel
+        { get { return this.defaultViewModel; } }
+        public NavigationHelper NavigationHelper
+        { get { return this.navigationHelper; } }
+        #endregion
+
+        private void _Start_bn_Click(object sender, RoutedEventArgs e)
+        {
+            bool bots = (bool)Bots_rb.IsChecked;
+            int amount = (int)_AmountOfPlayers_cb.SelectedItem;
+            Tuple<bool, int> info = new Tuple<bool, int>(bots, amount);
+            this.Frame.Navigate(typeof(FirstStateGame), e);
+        }
+    }
+}
